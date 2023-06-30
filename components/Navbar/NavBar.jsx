6f75bf9ce5c6d330589abe4f548545f91fbf7018
Prometheus2k <shonesk2000@gmail.com>
2023-06-30 22:19:49 +0530

    Navbar designing started

--- components/Navbar/NavBar.jsx
+++ components/Navbar/NavBar.jsx
@@ -14,7 +14,64 @@ import { Button } from "../componentindex";
 import images from "../../img";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  // ------------------ USESTATE ------------------
+  const [discover, setDiscover] = useState(false);
+  const [help, setHelp] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const openMenu = (e) => {
+    const btnText = e.target.innerText;
+    if (btnText === "Discover") {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else if (btnText === "Help Center") {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    } else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  };
+
+  return (
+    <div>
+      <div className={Style.navbar}>
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo}>
+            <Image
+              src={images.logo}
+              alt="NFT MARKET PLACE "
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={Style.navbar_container_left_box_input}>
+            <div className={Style.navbar_container_left_box_input_box}>
+              <input type="text" placeholder="Search NFT" />
+              <BsSearch onClick={() => {}} className={Style.search_icon} />
+            </div>
+          </div>
+        </div>
+        // ------------END OF LEFT SECTION----------------
+        <div className={Style.navbar_container_right}>
+          <div className={Style.navbar_container_right_discover}>
+            <p onClick={(e) => openMenu(e)}>Discover</p>
+            <div className={Style.navbar_container_right_discover_box}>
+              <Discover />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
