a73c44b80a40395ee253f8c3ec2445a00a294020
Prometheus2k <shonesk2000@gmail.com>
2023-07-08 23:11:14 +0530

    Added Hero Section

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -1,5 +1,6 @@
 import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import NavBar from "./Navbar/NavBar";
+import HeroSection from "./HeroSection/HeroSection";
 
-export { Button, Footer, NavBar };
+export { Button, Footer, NavBar, HeroSection };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+// INTERNAL IMPORTS
+import Style from "../styles/index.module.css";
+import { HeroSection } from "../components/componentsindex";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Home;
