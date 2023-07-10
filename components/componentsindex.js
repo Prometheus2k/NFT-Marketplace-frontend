c470ffeca94d700e0f3e0806c66cb33835d547b0
Prometheus2k <shonesk2000@gmail.com>
2023-07-10 12:18:02 +0530

    Added NFT Slider

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import NavBar from "./Navbar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
 
-export { Button, Footer, NavBar, HeroSection, Service };
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSlider };
