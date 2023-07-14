541c93b3e766b9f4db4ef76f1c1ee7dc04142727
Prometheus2k <shonesk2000@gmail.com>
2023-07-14 20:36:41 +0530

    Designed NFT product detail page

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-// INTERNAL IMPORT
+//INTERNAL IMPORT
 import Style from "./Button.module.css";
 
-const Button = ({ btnName, handleClick }) => {
+const Button = ({ btnName, handleClick, icon, classStyle }) => {
   return (
     <div className={Style.box}>
-      <button className={Style.button} onClick={() => handleClick()}>
-        {btnName}
+      <button
+        className={`${Style.button} ${classStyle}`}
+        onClick={() => handleClick()}
+      >
+        {icon} {btnName}
       </button>
     </div>
   );
