b82f953c4da780be018548692a74b2184b47d843
Prometheus2k <shonesk2000@gmail.com>
2023-07-19 22:05:19 +0530

    About us section

--- img/index.js
+++ img/index.js
@@ -48,8 +48,18 @@ import provider1 from "./provider-1.png";
 import provider2 from "./provider-2.png";
 import provider3 from "./provider-3.png";
 import provider4 from "./provider-4.png";
+import hero2 from "./hero2.png";
+import founder1 from "./founder1.jpg";
+import founder2 from "./founder2.jpg";
+import founder3 from "./founder3.jpg";
+import founder4 from "./founder4.jpg";
 
 export default {
+  founder1,
+  founder2,
+  founder3,
+  founder4,
+  hero2,
   upload,
   provider1,
   provider2,
