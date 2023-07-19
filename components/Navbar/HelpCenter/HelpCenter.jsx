56ad0aa5ab25bd1dd8c45f864b12cabe47661ea1
Prometheus2k <shonesk2000@gmail.com>
2023-07-19 20:30:27 +0530

    Login and Signup page

--- components/Navbar/HelpCenter/HelpCenter.jsx
+++ components/Navbar/HelpCenter/HelpCenter.jsx
@@ -16,11 +16,11 @@ const HelpCenter = () => {
     },
     {
       name: "Sign Up",
-      link: "sign-up",
+      link: "signUp",
     },
     {
       name: "Sign In",
-      link: "sign-in",
+      link: "login",
     },
     {
       name: "Subscription",
